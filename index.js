823535c0664e2484e1dd7954be1b44cf7d533e20
MikeZuck <105445747+MikeZuck1@users.noreply.github.com>
2023-08-14 13:22:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML;
 
-// veggies arrays
-const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+// veggies arrays => takes 3 elements (begin with the index 0)
+const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower']; 
 
-// create the function with one parameter
+// create the function with one parameter (veg)
 const politelyDecline = (veg) => {
   console.log('No ' + veg + ' please. I will have pizza with extra cheese.'); 
   // should print :
   // No broccoli, spinach, cauliflower, broccoflower please. I will have pizza with extra cheese.
 };
-politelyDecline(veggies); // call the function 
+politelyDecline(veggies); // call the function  
